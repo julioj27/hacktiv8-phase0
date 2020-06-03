943e81712d7b0f45e90b227b7dae064b12a391dc
julioj27 <juliojaya27@gmail.com>
2020-06-03 15:35:13 +0700

    Add write up for holiday problem #2

---
+++ 2.js
@@ -0,0 +1,58 @@
+/*
+==============
+Favorite Color
+==============
+
+[INSTRUCTIONS]
+Kita tahu bahwa warna dasar adalah: 'red', 'yellow', 'blue'
+Pada sebuah panelis pemilihan warna, function akan me-return output warna terbanyak.
+Apabila jumlah 'red' dalam array lebih besar dari jumlah 'yellow' dan 'blue', maka function akan mereturn "red the most favorite!".
+Apabila jumlah 'yellow' dalam array lebih besar dari jumlah 'red' dan 'blue', maka function akan mereturn "yellow the most favorite!".
+Apabila jumlah 'blue' dalam array lebih besar dari jumlah 'red' dan 'yellow', maka function akan mereturn "blue the most favorite!".
+Apabila ada jumlah yang sama dan jumlah tersebut merupakan jumlah terbesar, tampilkan "There are no clear winner!"
+
+[RULE]
+- Wajib menuliskan algoritma/pseudocode.
+- Dilarang menggunakan Regex (.match, .test, dan sebagainya)
+- Dilarang menggunakan method .filter, atau .reduce!
+*/
+
+//pseudocode here
+
+function favoriteColor (input) {
+  // your code here!
+  var result = '';
+  var temp = {};
+  var highestFrequency = 0;
+  var mode = '';
+  if(input.length !== 0) {
+    for(let i = 0; i < input.length; i++) {
+      var color = input[i];
+      if(temp[color] === undefined) {
+        temp[color] = 1;
+      } else {
+        temp[color] += 1;
+      }
+    }
+    // console.log(temp);
+    for(var key in temp) {
+      if(temp[key] > highestFrequency) {
+        highestFrequency = temp[key];
+        mode = key;
+        result = `${mode} the most favorite!`;
+      } else if(temp[key] === highestFrequency) {
+        result = `There are no clear winner!`;
+      }
+    }
+  } else {
+    result = `There are no favorite!`;
+  }
+
+  return result;
+}
+
+console.log(favoriteColor(['red', 'red', 'yellow'])); // "red the most favorite!"
+console.log(favoriteColor(['blue', 'red', 'yellow', 'blue'])); // "blue the most favorite!"
+console.log(favoriteColor(['yellow', 'yellow', 'yellow'])); // "yellow the most favorite!"
+console.log(favoriteColor(['red'])); // "red the most favorite!"
+console.log(favoriteColor([])); // "There are no favorite!"
